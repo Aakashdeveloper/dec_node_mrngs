923704d28f193be10bb38b46679f7b0985f47ba8
aakashdeveloper <ahanda205@gmail.com>
2020-12-08 06:58:52 +0530

    A

--- fullstack/app.js
+++ fullstack/app.js
@@ -1,53 +1,24 @@
 var express = require('express');
 var app = express();
 var port = 8900;
-var data = [
-    {
-      "_id": 1,
-      "city_name": "Delhi",
-      "city": 1,
-      "country_name": "India"
-    },
-    {
-      "_id": 3,
-      "city_name": "Pune",
-      "city": 3,
-      "country_name": "India"
-    },
-    {
-      "_id": 2,
-      "city_name": "Mumbai",
-      "city": 2,
-      "country_name": "India"
-    },
-    {
-      "_id": 4,
-      "city_name": "Chandigarh",
-      "city": 4,
-      "country_name": "India"
-    },
-    {
-      "_id": 5,
-      "city_name": "Goa",
-      "city": 5,
-      "country_name": "India"
-    },
-    {
-      "_id": 6,
-      "city_name": "Manali",
-      "city": 6,
-      "country_name": "India"
-    }
-  ]
+var hotelRouter = require('./src/routes/hotelRoutes');
+var cityRouter = require('./src/routes/cityRoutes');
+
+//static file path
+app.use(express.static(__dirname+'/public'));
+//html
+app.set('views','./src/views');
+//view engine
+app.set('view engine','ejs')
 
 
 app.get('/',function(req,res){
-    res.send('Welcome to express')
+    //res.send('Welcome to express')
+    res.render('index',{title:'Home Page'})
 });
 
-app.get('/movies',function(req,res){
-    res.send(data)
-})
+app.use('/hotel',hotelRouter);
+app.use('/city',cityRouter);
 
 app.listen(port,function(err){
     if(err) throw err;
