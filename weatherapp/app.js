6947e306199236ef018dafd0f1b5bea5f0195718
aakashdeveloper <ahanda205@gmail.com>
2020-12-17 06:50:06 +0530

    A

---
+++ weatherapp/app.js
@@ -0,0 +1,33 @@
+const { response } = require('express');
+var express = require('express');
+var request = require('request');
+var app = express();
+var port = process.env.PORT || 7888;
+
+var apiurl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Paris&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29"
+
+
+//static file path
+app.use(express.static(__dirname+'/public'));
+//html
+app.set('views','./src/views');
+//view engine
+app.set('view engine','ejs');
+
+app.get('/weather',function(req,res){
+    var city = req.query.city;
+    apiurl = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+    console.log(apiurl)
+    request(apiurl,(err,response) => {
+        if(err) throw err;
+        var output = JSON.parse(response.body)
+        //var myobj = {city:output.city.name,data:output.list}
+        //res.send(myobj)
+        res.render('index',{title:'Weather App',result:output})
+    })
+})
+
+app.listen(port,(err) => {
+    if(err) throw err;
+    console.log("Server is running on port "+port)
+})
