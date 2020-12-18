6f52afa2e635f6e4bfd732ceada2fd672847c7a1
aakashdeveloper <ahanda205@gmail.com>
2020-12-18 07:24:03 +0530

    a

--- weatherapp/app.js
+++ weatherapp/app.js
@@ -1,11 +1,10 @@
-const { response } = require('express');
-var express = require('express');
-var request = require('request');
-var app = express();
-var port = process.env.PORT || 7888;
-
-var apiurl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Paris&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29"
-
+//var express = require('express');
+//var request = require('request');
+import express from 'express'
+import request from 'request'
+let app = express();
+let port = process.env.PORT || 7888;
+let apiurl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Paris&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29"
 
 //static file path
 app.use(express.static(__dirname+'/public'));
@@ -14,15 +13,13 @@ app.set('views','./src/views');
 //view engine
 app.set('view engine','ejs');
 
-app.get('/weather',function(req,res){
-    var city = req.query.city;
+app.get('/weather',(req,res) =>{
+    let city = req.query.city;
     apiurl = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
     console.log(apiurl)
     request(apiurl,(err,response) => {
         if(err) throw err;
-        var output = JSON.parse(response.body)
-        //var myobj = {city:output.city.name,data:output.list}
-        //res.send(myobj)
+        let output = JSON.parse(response.body)
         res.render('index',{title:'Weather App',result:output})
     })
 })
@@ -31,3 +28,7 @@ app.listen(port,(err) => {
     if(err) throw err;
     console.log("Server is running on port "+port)
 })
+
+
+ //var myobj = {city:output.city.name,data:output.list}
+ //res.send(myobj)
